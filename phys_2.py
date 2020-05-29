c79f7dbaafab6e258128bf2a70422183c8ffae14
maaxxaam <65228215+maaxxaam@users.noreply.github.com>
2020-05-29 15:01:48 +0300

    Update phys_2.py

--- phys_2.py
+++ phys_2.py
@@ -10,7 +10,12 @@ E0 = 27.07
 E = 28
 eps = -E/E0
 beta = (2*eps)**0.5
-z = n*beta
+z = 1
 e = 1.6*10**(-19)
+a = 0.529
+def lagger_a_b(a,b):
+    d_a = a * 10**(-10)
+    d_ab = a*b * 10**(-10)
+    
 Anl = 1/fact(2*l+1)*(fact(n+l)/(2*n*fact(n-l+1)))**0.5*(2*z/n)**1.5
 print(Anl)
