cbb202982422055c1bbc9b8abe9a3bd9696f8abc
maaxxaam <65228215+maaxxaam@users.noreply.github.com>
2020-05-29 16:57:27 +0300

    Update phys_2.py
    Added working (sort of) Lagger function

--- phys_2.py
+++ phys_2.py
@@ -1,5 +1,5 @@
 #trying to do some project calcs
-import math
+from sympy import *
 def fact(a):
     if a <= 1 : return 1
     else: return a*fact(a-1)
@@ -13,9 +13,16 @@ beta = (2*eps)**0.5
 z = 1
 e = 1.6*10**(-19)
 a = 0.529
-def lagger_a_b(a,b):
-    d_a = a * 10**(-10)
-    d_ab = a*b * 10**(-10)
+def lagger(al, ga, ze):
+    '''
+Calculates Lagger's polinominal. Takes three arguments: alpha, gamma and zeta
+    '''
+    el = 1.6*10**(-19)
+    alpha, gamma, zeta, ele = symbols('alpha gamma zeta ele')
+    expr = diff(ele**zeta * diff(zeta**gamma * ele**(-zeta), zeta, ga), zeta, al)
+    func = lambdify(['alpha','gamma','zeta','ele'],expr)
+    alpha, gamma, zeta, ele = al, ga, ze, el
+    return func(alpha, gamma, zeta, el)
     
 Anl = 1/fact(2*l+1)*(fact(n+l)/(2*n*fact(n-l+1)))**0.5*(2*z/n)**1.5
-print(Anl)
+print(lagger(1,1,1))
