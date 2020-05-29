b6c56e0bf2880b880fa0544936b8fd15767c87bf
maaxxaam <65228215+maaxxaam@users.noreply.github.com>
2020-05-29 14:25:37 +0300

    Update phys_2.py

--- phys_2.py
+++ phys_2.py
@@ -5,7 +5,12 @@ def fact(a):
     else: return a*fact(a-1)
 n = 1
 l = 0
-z = 1
 eps = 1
-Anl = 1/fact(2*l+1)*(fact(n+l)/(2*n*fact(n-l+1)))**1/2*(2*z/n)**3/2
+E0 = 27.07
+E = 28
+eps = -E/E0
+beta = (2*eps)**0.5
+z = n*beta
+e = 1.6*10**(-19)
+Anl = 1/fact(2*l+1)*(fact(n+l)/(2*n*fact(n-l+1)))**0.5*(2*z/n)**1.5
 print(Anl)
